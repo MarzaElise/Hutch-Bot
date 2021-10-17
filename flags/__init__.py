ca8e95ec39f9bf180475cb60f167fbaee5460a99
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-17 21:45:53 +0530

    Notice in flags/

--- flags/__init__.py
+++ flags/__init__.py
@@ -1,2 +1,5 @@
 from ._command import *
 from ._parser import *
+
+# i didnt write any of this code, i copied it from the original discord.ext.flags module
+# which was removed when discord.py was discontinued
