041e8faae371ff494a46630813ca18322875f7f5
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-28 13:21:44 +0530

    Remove flags/ in favor of discord.ext.commands.flags

--- flags/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._command import *
-from ._parser import *
-
-# i didnt write any of this code, i copied it from the original discord.ext.flags module
-# which was removed when discord.py was discontinued
