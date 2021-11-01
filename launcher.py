e7f5502bc300d29efadf694d47aa3d33afb18c49
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-11-01 11:02:13 +0530

    Change prefix to > when running test bot

--- launcher.py
+++ launcher.py
@@ -5,9 +5,12 @@ from Bot import MyBot
 
 
 def main(TOKEN: str = "TOKEN_2"):
+    bot = MyBot(TOKEN)
+    change_status.start(bot)
+    if TOKEN == "TOKEN_2":
+        bot.command_prefix = ">"
+        print(f"\n[INFO]: Running Test bot with {bot.command_prefix} prefix")
     try:
-        bot = MyBot(TOKEN)
-        change_status.start(bot)
         bot.run(bot.config.BOT_TOKEN)
     except Exception as e:
         print(f"[ERROR]: {e.__class__.__name__} \n{traceback.format_exc()}")
